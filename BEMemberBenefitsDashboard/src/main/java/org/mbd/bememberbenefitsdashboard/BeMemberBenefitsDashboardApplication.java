ebe9c9d7f82837e35493f3f595c76e0bff290187
stan10aviles <stan10aviles@hotmail.com>
2025-10-13 12:27:49 -0400

    Initialized backend spring boot project

---
+++ BEMemberBenefitsDashboard/src/main/java/org/mbd/bememberbenefitsdashboard/BeMemberBenefitsDashboardApplication.java
@@ -0,0 +1,13 @@
+package org.mbd.bememberbenefitsdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMemberBenefitsDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMemberBenefitsDashboardApplication.class, args);
+	}
+
+}
---
+++ BEMemberBenefitsDashboard/src/test/java/org/mbd/bememberbenefitsdashboard/BeMemberBenefitsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mbd.bememberbenefitsdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMemberBenefitsDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
